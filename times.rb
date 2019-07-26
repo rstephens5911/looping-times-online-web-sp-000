90a55ffe5f795fc77b9756aadbd79c100c33c25f
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 01:46:40 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,19 +1,5 @@
 def using_times
-	#your code here
+  7.times do
+    puts "Wingardium Leviosa"
+  end
 end
-
-
-
-def jewels
-  jewels_in_bag = 100
-  3.times do
-    puts "hiding 10 stolen jewels."
-    jewels_in_bag = jewels_in_bag - 10
-    puts "now there are only #{jewels_in_bag} left to hide!"
-  end
-
-  puts "we have #{jewels_in_bag} jewels still to hide!"
-
-  end
-
-  jewels
