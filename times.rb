a6a48265910ed426c2597d24b93f62599ecad59e
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 01:45:17 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -8,7 +8,7 @@ def jewels
   jewels_in_bag = 100
   3.times do
     puts "hiding 10 stolen jewels."
-    jelwels_in_bag = jewels_in_bag - 10
+    jewels_in_bag = jewels_in_bag - 10
     puts "now there are only #{jewels_in_bag} left to hide!"
   end
 
